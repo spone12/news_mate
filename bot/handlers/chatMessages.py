d24f976b47446fc0570151d7dcc170c289fb911c
Spone <spone185@gmail.com>
2024-10-01 20:24:02 +0300

    Inline keyboard class added

--- bot/handlers/chatMessages.py
+++ bot/handlers/chatMessages.py
@@ -1,13 +1,38 @@
-from aiogram import Router, F
+from aiogram import Router, F, types
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from bot.news.news import *
+from bot.keyboards.inline import *
+
 
 chatMessagesRouter = Router()
 
 @chatMessagesRouter.message(CommandStart())
 async def cmdStart(message: Message):
-    await message.answer('Добро пожаловать в бот!')
+
+    keyboard = InlineKeyboard()
+    
+    LEXICON: dict[str, str] = {
+    'but_1': 'Кнопка 1',
+    'but_2': 'Кнопка 2',
+    'but_3': 'Кнопка 3',
+    'but_4': 'Кнопка 4',
+    'but_5': 'Кнопка 5',}
+
+    BUTTONS: dict[str, str] = {
+        'btn_1': '1',
+        'btn_2': '2',
+        'btn_3': '3',
+        'btn_4': '4',
+        'btn_5': '5',}
+
+    keyboard = keyboard.create_inline_kb(2, LEXICON, BUTTONS)
+    
+    await message.answer(
+        text='Inline keyboard '
+             '<code>create_inline_kb</code>',
+        reply_markup=keyboard
+    )
 
 @chatMessagesRouter.message(Command('get_news'))
 async def cmdGetNews(message: Message):
@@ -15,3 +40,8 @@ async def cmdGetNews(message: Message):
     newsArray = News().sendMessage()
     for news in newsArray:
         await message.answer(news)
+
+@chatMessagesRouter.callback_query()
+async def callback_handle(call: types.CallbackQuery):
+    print(call)
+    #await call.answer()
--- bot/keyboards/inline.py
+++ bot/keyboards/inline.py
@@ -1,1 +1,40 @@
 # Inline-keyboards for dynamic actions
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+
+class InlineKeyboard():
+
+    def __init__(self):
+        self.builder = InlineKeyboardBuilder() 
+
+    def create_inline_kb(self,
+                         width: int,
+                         buttonNames: str,
+                         buttonActions: str,
+                        **kwargs: str
+                        ) -> InlineKeyboardMarkup:
+        
+        # Initialize the builder
+        kbBuilder = InlineKeyboardBuilder()
+        # Initialize the list for buttons
+        buttons: list[InlineKeyboardButton] = []
+
+        if buttonNames:
+            for button in buttonNames:
+                buttons.append(InlineKeyboardButton(
+                    text=buttonActions[button] if button in buttonActions else button,
+                    callback_data=button)
+                )
+
+        if kwargs:
+            for button, text in kwargs.items():
+                buttons.append(InlineKeyboardButton(
+                    text=text,
+                    callback_data=button))
+
+        # Unpack the list with buttons into the builder using the row method with the width parameter
+        kbBuilder.row(*buttons, width = width)
+
+        # Return an inline keyboard object
+        return kbBuilder.as_markup()
+    
